465ba62cd201ac7eb61903ee4eb6cd8ad75476da
jaiyankargupta <jaiyankargupta@gmail.com>
2025-02-14 22:37:10 +0530

    14 feb done

---
+++ 14Feb.cpp
@@ -0,0 +1,32 @@
+//question no : 1352 (LC)
+class ProductOfNumbers {
+
+vector<int> stream = {1};
+int n ;
+int zero_idx; 
+public:
+    ProductOfNumbers() {
+        n = 1;
+       zero_idx = -1;
+    }
+    
+    void add(int num) {
+        if(num==0) zero_idx = n;
+        if(stream.back()==0) stream.push_back(num);
+        else stream.push_back(stream.push_back()*num);
+        n++;
+    }
+    
+    int getProduct(int k) {
+        if(stream[n-k-1] == 0) return stream[n-1];
+        if(zero_idx >= n-k) retur 0;
+       else return stream[n-k-1]/stream[n-k];
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
